ded248a0866b67f3325ac0a61a9a5df5e59cedf0
Yurii_Yevchenko <yurievch@gmail.com>
2024-02-26 12:38:50 +0200

    контекст в React

--- src/App.js
+++ src/App.js
@@ -5,18 +5,23 @@ import Home from './pages/Home';
 import Cart from './pages/Cart';
 import NotFound from './pages/NotFound';
 import { Routes, Route } from 'react-router-dom';
+
+export const SearchContext = React.createContext();
+console.log(SearchContext);
 function App() {
   const [searchValue, setSearchValue] = React.useState('');
   return (
     <div className="wrapper">
-      <Header searchValue={searchValue} setSearchValue={setSearchValue} />
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home searchValue={searchValue} />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
+      <SearchContext.Provider value={{ searchValue, setSearchValue }}>
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </SearchContext.Provider>
     </div>
   );
 }
